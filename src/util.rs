744d05299f2a6fbd9207b798484dccaa7a648327
Dragoș Tiselice <dragos@rive.app>
2023-12-13 19:19:12 +0200

    Disabled VSync.

--- src/util.rs
+++ src/util.rs
@@ -78,7 +78,7 @@ impl RenderContext {
             format,
             width,
             height,
-            present_mode: wgpu::PresentMode::AutoVsync,
+            present_mode: wgpu::PresentMode::AutoNoVsync,
             alpha_mode: wgpu::CompositeAlphaMode::Auto,
             view_formats: vec![],
         };
